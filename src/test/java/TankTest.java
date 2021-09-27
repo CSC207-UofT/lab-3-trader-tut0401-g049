22803b36ec638c2d7c03f5e1ea9d0f3cd3ef90d7
onyxc <onyx.christopher@mail.utoronto.ca>
2021-09-27 17:41:25 -0400

    Pre-review commit

---
+++ src/test/java/TankTest.java
@@ -0,0 +1,36 @@
+/*
+ * This file contains sample JUnit test cases for Horse.java
+ * You should NOT modify this file!
+ */
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+
+public class TankTest {
+    Tank t;
+
+    @Before
+    public void setUp() {
+        t = new Tank();
+    }
+
+    @Test(timeout = 50)
+    public void TestGetMaxSpeed() {
+        assertEquals(3, t.getMaxSpeed());
+    }
+
+    @Test(timeout = 50)
+    public void TestUpgradeSpeed() {
+        t.upgradeSpeed();
+        assertEquals(4, t.getMaxSpeed());
+    }
+
+    @Test(timeout = 50)
+    public void TestDowngradeSpeed() {
+        t.downgradeSpeed();
+        assertEquals(2, t.getMaxSpeed());
+    }
+
+}
