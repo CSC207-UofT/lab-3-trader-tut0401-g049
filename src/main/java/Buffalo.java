208f120f3432f04f68dd21950d9f308f156d1f33
DivitSingh <divit.singh@mail.utoronto.ca>
2021-09-27 17:41:06 -0400

    Buffalo!

---
+++ src/main/java/Buffalo.java
@@ -0,0 +1,38 @@
+public class Buffalo implements Drivable, Domesticatable, Tradable {
+    private int maxSpeed;
+
+    public Buffalo() {
+        this.maxSpeed = 1;
+    }
+
+    @Override
+    public String sound() {
+        return "Neigh!";
+    }
+
+    @Override
+    public void upgradeSpeed() {
+        this.maxSpeed++;
+    }
+
+    @Override
+    public void downgradeSpeed() {
+        this.maxSpeed--;
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return this.maxSpeed;
+    }
+
+    @Override
+    public int getPrice() {
+        return 10;
+    }
+
+    @Override
+    public String toString() {
+        String id = super.toString();
+        return id + " (Max speed: " + this.maxSpeed + ")";
+    }
+}
